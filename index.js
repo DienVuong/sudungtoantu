63d2066f5052e02a0a7f619a7590b22c9b06d55c
DienVuong <vuongdien261@gmail.com>
2022-09-08 11:12:52 +0700

    lan 1

---
+++ index.js
@@ -0,0 +1,6 @@
+let width;
+let height;
+width= +prompt( 'nhap chieu rong hinh chu nhat');
+height=+prompt('nhap chieu dai hinh chu nhat');
+let area = width * height ;
+document.write('dien tich hinh chu nhat :'+area);
